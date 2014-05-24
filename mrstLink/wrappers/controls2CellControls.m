c628ec000b5dcaf29827b7bd7708a6ecae6fd954
Andres Codas <andrescodas@itk.ntnu.no>
2014-05-24 14:31:33 +0200

    simplify function

--- mrstLink/wrappers/controls2CellControls.m
+++ mrstLink/wrappers/controls2CellControls.m
@@ -3,18 +3,11 @@ function [vals] = controls2CellControls(u,schedule)
 %  divide the controls in cells according to the number of wells
 %
 
-useMrstSchedule = isfield(schedule.control(1), 'W');
-
 % assuming same number of wells during the schedule
-if useMrstSchedule
-    numC = numel(schedule.control);
-    numW  = numel(schedule.control(1).W);
-else
-    numC = numel(schedule.control);
-    numW  = size(schedule.control(1).WCONINJE, 1)+size(schedule.control(1).WCONPROD, 1);
-end
+nW  = getnW( schedule );
+nC = numel(schedule.control);
 
-vals = mat2cell(u,numW*ones(numC,1),1);
+vals = mat2cell(u,nW*ones(nC,1),1);
 
 
 
