fde864a5add9f34e6be849485bd820dc5e6aa51e
Andres Codas <andrescodas@itk.ntnu.no>
2014-08-08 10:07:31 +0200

    remove limits in the schedule

---
+++ mrstLink/utils/relaxLimsInSchedule.m
@@ -0,0 +1,24 @@
+function [ schedule ] = relaxLimsInSchedule( schedule)
+
+
+for k = 1:length(schedule.control)
+    for w = 1:length(schedule.control(k).W)
+        
+        lims = struct();
+        if schedule.control(k).W(w).sign == 1  %% injector
+            lims.rate = inf;
+            lims.bhp = inf;
+        else  % producer
+            lims.orat = -inf;
+            lims.wrat = -inf;
+            lims.grat = -inf;
+            lims.lrat = -inf;
+            lims.bhp  = 0;
+        end
+        schedule.control(k).W(w).lims = lims;
+    end
+end
+
+
+end
+
