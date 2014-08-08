7413a04a39f3f3f461c0999ce5fb6436b4269004
Andres Codas <andrescodas@itk.ntnu.no>
2014-08-08 10:02:18 +0200

    Transfrom deck-control to well-control

---
+++ mrstLink/utils/eclipseSchedule2mrstSchedule.m
@@ -0,0 +1,20 @@
+function [schedule] = eclipseSchedule2mrstSchedule(schedule,G,rock)
+%
+% Transform a eclipse schedule to a mrst schedule
+%
+useMrstSchedule = isfield(schedule.control(1), 'W');
+
+if ~useMrstSchedule
+    nc = numel(schedule.control);
+    mrstControl = repmat(struct('W',[]),nc,1);
+    for k = 1:nc
+        % processWellsLocal is call as in runScheduleADI
+        mrstControl(k).W =  processWellsLocal(G, rock, schedule.control(k),...
+            'Verbose', true, 'DepthReorder', false);
+    end
+    schedule.control = mrstControl;
+    
+end
+
+
+end
