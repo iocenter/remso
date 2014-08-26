47abbfc4e5355e5f4fbaa01645dc3fff4494350c
Andres Codas <andrescodas@itk.ntnu.no>
2014-08-26 13:04:31 +0200

    Condensing algorithm improvement

--- optimization/remsoSequential/condensing.m
+++ optimization/remsoSequential/condensing.m
@@ -36,11 +36,11 @@ function varargout = condensing(x,u,v,ss,varargin)
 %   xd - Difference xs-x. (Multiple Shooting error)
 %
 %   vd - Difference vs-v. (Multiple Shooting error)
-% 
+%
 %   ax Ax - A predicition model for the states
 %           \Delta x = x + ax + Ax * \Delta u
 %
-%   av Av - A predicition model for the algebraic states 
+%   av Av - A predicition model for the algebraic states
 %           \Delta v = v + av + Av * \Delta u
 %
 % SEE ALSO:
@@ -74,12 +74,19 @@ else
     simVars = opt.simVars;
 end
 
-dzdd = cell(1,totalPredictionSteps);
-ax = cell(totalPredictionSteps,1);
-Ax = cell(totalPredictionSteps,totalControlSteps);
+dzdd = zeros(nx,1);
 
-av = cell(totalPredictionSteps,1);
-Av = cell(totalPredictionSteps,totalControlSteps);
+ax = zeros(nx*totalPredictionSteps,1);
+ax = mat2cell(ax,nx*ones(totalPredictionSteps,1),1);
+
+Ax = zeros(nx*totalPredictionSteps,nu*totalControlSteps);
+Ax = mat2cell(Ax,nx*ones(totalPredictionSteps,1),nu*ones(1,totalControlSteps));
+
+av = zeros(nv*totalPredictionSteps,1);
+av = mat2cell(av,nv*ones(totalPredictionSteps,1),1);
+
+Av = zeros(nv*totalPredictionSteps,nu*totalControlSteps);
+Av = mat2cell(Av,nv*ones(totalPredictionSteps,1),nu*ones(1,totalControlSteps));
 
 
 converged = false(totalPredictionSteps,1);
@@ -88,79 +95,78 @@ converged = false(totalPredictionSteps,1);
 t0 = tic;
 k0 = 0;
 for k = 1:totalPredictionSteps
-	[t0,k0] = printCounter(1, totalPredictionSteps, k,'Condensing',t0,k0);
-
+    [t0,k0] = printCounter(1, totalPredictionSteps, k,'Condensing',t0,k0);
+    
     
     i = callArroba(ss.ci,{k});
     ui = u{i};
     
     % Prepare the RHS for the condensing technique
+    
     if k >1
-        im = callArroba(ss.ci,{k-1});
-        xRightSeeds = [Ax(k-1,1:im),dzdd(1:k-1)];
-        seedSizes =  cellfun(@(x)size(x,2),xRightSeeds);
-        xRightSeeds = [cell2mat(xRightSeeds),zeros(nx,nu)];
-        nxR = size(xRightSeeds,2)-nu;
-        uRightSeeds = [zeros(nu,nxR),eye(nu)];
+        xRightSeeds = [{dzdd}       ,Ax(k-1,1:i)];
+        seedSizes = cellfun(@(x)size(x,2),xRightSeeds);   
+        xRightSeeds = cell2mat(xRightSeeds);
+        
+        uRightSeeds = [zeros(nu,1+nu*(i-1)), eye(nu)];
+
         xStart = x{k-1};
+        
     elseif  k == 1
-        im = [];
-        nxR = 0;
-        xRightSeeds = zeros(nx,nu);
+        
+        xRightSeeds = zeros(nx,nu);      
         uRightSeeds = eye(nu);
+
         xStart = ss.state;
+        
     else
         error('what?')
     end
+    
+
     
     % Compute the Jacobian-vector products
     [xs{k},vs{k},Jac,convergence] = ss.step{k}(xStart,ui,'gradients',true,'xRightSeeds',xRightSeeds,'uRightSeeds',uRightSeeds,'simVars',simVars{k});
     
     converged(k) = convergence.converged;
-
-        xd{k} = (xs{k}-x{k});
-        if withAlgs
-            vd{k} = (vs{k}-v{k});
-        end
+    
+    xd{k} = (xs{k}-x{k});
+    if withAlgs
+        vd{k} = (vs{k}-v{k});
+    end
     
     % Extract the linearized model information
     if k >1
-        [AxDzdd] = mat2cell(Jac.xJ(:,1:nxR),nx,seedSizes);
-        [Ax{k,1:im},dzdd{1:k-1}] = deal(AxDzdd{:});
-    end
-    dzdd{k} = -xd{k};
-    if ~isempty(Ax{k,i})
-        Ax{k,i} = Ax{k,i}+Jac.xJ(:,nxR+1:end);
-    else
-        Ax{k,i} = Jac.xJ(:,nxR+1:end);
+        [dzddAx] = mat2cell(Jac.xJ,nx,seedSizes);
+        [dzdd,Ax{k,1:i}] = deal(dzddAx{:});
+    else % k ==1
+        [Ax{1,1}] = Jac.xJ;        
     end
     
-    ax{k}    = -sum(cat(2,dzdd{1:k}),2);
+    dzdd = dzdd -xd{k};
+    
+    ax{k}    = -dzdd;
     
     if withAlgs
         if k >1
-            [dvddvsu] = mat2cell(Jac.vJ(:,1:nxR),nv,seedSizes);
-            [Av{k,1:im}] = deal(dvddvsu{1:im});
+            [dvddvsu] = mat2cell(Jac.vJ,nv,seedSizes);
+            [dvdd,Av{k,1:i}] = deal(dvddvsu{:});
             
-            av{k}    = vd{k}-sum(cat(2,dvddvsu{im+1:end}),2);
-        elseif k==1
+            av{k}    = vd{k}-dvdd;
+        else %k==1
+            Av{1,1} = Jac.vJ;
             av{1}    = vd{k};
         end
         
-        if ~isempty(Av{k,i})
-            Av{k,i} = Av{k,i}+Jac.vJ(:,nxR+1:end);
-        else
-            Av{k,i} = Jac.vJ(:,nxR+1:end);
-        end
     end
     
     
 end
 
- if ~all(converged) 
-     steps = 1:totalPredictionSteps;
-     warning(strcat('Steps failed to converge:',num2str(steps(~converged))));
- end
+if ~all(converged)
+    steps = 1:totalPredictionSteps;
+    warning(strcat('Steps failed to converge:',num2str(steps(~converged))));
+end
 
 varargout{1} = xs;
 varargout{2} = vs;
