24f55571e93573f413b3270e71f2e6704baad4a3
Thiago Lima <thiagolims@gmail.com>
2014-11-24 18:08:25 -0200

    include netSol on output function interface

--- mrstLink/wrappers/mrstTimePointFuncWrapper.m
+++ mrstLink/wrappers/mrstTimePointFuncWrapper.m
@@ -1,4 +1,4 @@
-function [f,Jac] = mrstTimePointFuncWrapper(xfk,uk,vk,target,schedule,wellSol,varargin)
+function [f,Jac] = mrstTimePointFuncWrapper(xfk,uk,vk,target,schedule,wellSol,netSol,varargin)
 %
 % Interface with Remso a mrstPointFunction
 %
@@ -44,22 +44,23 @@ function [f,Jac] = mrstTimePointFuncWrapper(xfk,uk,vk,target,schedule,wellSol,va
 opt = struct('partials',false,'leftSeed',[],'xScale',[],'vScale',[],'uScale',[],'xRightSeeds',[],'uRightSeeds',[],'vRightSeeds',[]);
 opt = merge_options(opt, varargin{:});
 
-[wellSol] = algVar2wellSol(vk,wellSol,'vScale',opt.vScale);
+[wellSol,netSol,JacTW,JacTN] = algVar2mrstAlg(vk,wellSol,netSol,'vScale',opt.vScale,'partials',opt.partials);
 [ state,JacTX] = stateVector2stateMrst( xfk,'xScale',opt.xScale,...
     'partials',opt.partials);
 [ schedule,JacTU ] = controls2Schedule( uk,schedule,'uScale',opt.uScale,...
     'partials',opt.partials);
 
 
-targetObj = callArroba(target,{state,wellSol,schedule},'ComputePartials', opt.partials,'leftSeed',opt.leftSeed);
+targetObj = callArroba(target,{state,wellSol,netSol,schedule},'ComputePartials', opt.partials,'leftSeed',opt.leftSeed);
     
 
 f = double(targetObj);
 Jac = [];
 if opt.partials
-    Jv = bsxfun(@times,cell2mat(targetObj.jac(3:5)),opt.vScale');
-    Jx = cell2mat(targetObj.jac(1:2))*JacTX;
-    Ju = cell2mat(targetObj.jac(6))*JacTU;
+    %% TODO:  index in 1 2 3 4 (x,w,n,u)
+	Jx = cell2mat(targetObj.jac(1:2))*JacTX;
+    Jv = [cell2mat(targetObj.jac(3:5))*JacTW,cell2mat(targetObj.jac(6))*JacTN];
+    Ju = cell2mat(targetObj.jac(7))*JacTU;
     
     if ~(size(opt.xRightSeeds,1)==0)
         Jac.J = Jx*opt.xRightSeeds + Ju*opt.uRightSeeds + Jv*opt.vRightSeeds;
