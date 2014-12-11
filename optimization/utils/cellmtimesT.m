0e65a9bbde669dc4470aa2524480ccfbf1c7abef
Andres Codas <andrescodas@itk.ntnu.no>
2014-12-11 10:00:42 +0100

    Add option to treat vector dimension

--- optimization/utils/cellmtimesT.m
+++ optimization/utils/cellmtimesT.m
@@ -1,7 +1,8 @@
 function [ y ] = cellmtimesT( x,A,varargin )
 %  y = x'A, exploit the structure of the predictor matrix
+%  columnVector == true then x is a column vector, otherwise it is a line
 
-opt = struct('lowerTriangular',false,'ci',[]);
+opt = struct('lowerTriangular',false,'ci',[],'columnVector',true);
 opt = merge_options(opt, varargin{:});
 
 if isempty(opt.ci)
@@ -13,17 +14,37 @@ end
 nC = size(A{1,1},2);
 y = repmat({zeros(1,nC)},1,nA);
 
-if opt.lowerTriangular
-    for iA = 1:mA
-        maxjA = callArroba(opt.ci,{iA});
-        for jA = 1:maxjA   %jA = jy
-            y{jA} = y{jA} + x{iA}'*A{iA,jA};
-        end
-    end    
-else
-    for jA = 1:nA   %jA = jy
+if opt.columnVector
+    if opt.lowerTriangular
         for iA = 1:mA
-            y{jA} = y{jA} + x{iA}'*A{iA,jA};
+            maxjA = callArroba(opt.ci,{iA});
+            for jA = 1:maxjA   %jA = jy
+                y{jA} = y{jA} + x{iA}'*A{iA,jA};
+            end
+        end
+    else
+        for jA = 1:nA   %jA = jy
+            for iA = 1:mA
+                y{jA} = y{jA} + x{iA}'*A{iA,jA};
+            end
         end
     end
+else
+    if opt.lowerTriangular
+        for iA = 1:mA
+            maxjA = callArroba(opt.ci,{iA});
+            for jA = 1:maxjA   %jA = jy
+                y{jA} = y{jA} + x{iA}*A{iA,jA};
+            end
+        end
+    else
+        for jA = 1:nA   %jA = jy
+            for iA = 1:mA
+                y{jA} = y{jA} + x{iA}*A{iA,jA};
+            end
+        end
+    end
+    
+    
 end
+
