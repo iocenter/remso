03f1dce41e2ec739da9cdc4e86d959e4b6cb2370
Andres Codas <andrescodas@itk.ntnu.no>
2015-01-15 22:48:58 +0100

    inclue a tolerance to activate warning

--- optimization/remsoCrossSequential/checkBounds.m
+++ optimization/remsoCrossSequential/checkBounds.m
@@ -1,20 +1,32 @@
-function  [ok,x]  = checkBounds( lbx,x,ubx,varargin)
+function  [ok,xN]  = checkBounds( lbx,x,ubx,varargin)
 
-opt = struct('chopp',false,'verbose',false);
+opt = struct('chopp',false,'verbose',false,'tol',1e-10);
 opt = merge_options(opt, varargin{:});
 
-
+xN = x;
 ok = cellfun(@(l,v,u) all(l<=v) && all(v<=u),lbx,x,ubx);
 
 if ~all(ok)
+    
     if opt.chopp
+
+        xN = cellfun(@(l,v,u) min(max(l,v),u),lbx,x,ubx,'UniformOutput',false);
+        
+
         if opt.verbose
-            warning('Variables out of bounds -> chopping varibles')
+            error = sum(cellfun(@(x1,x2)sum(abs(x1-x2)),xN,x));
+            if error > opt.tol
+                warning(['Variables out of bounds -> chopping varibles, Violation norm1 = ',num2str(error)])
+            end
         end
-        x = cellfun(@(l,v,u) min(max(l,v),u),lbx,x,ubx,'UniformOutput',false);
+        
+        
     else
         if opt.verbose
-            warning('Variables out of bounds')
+            error = sum(cellfun(@(x1,x2)sum(abs(x1-x2)),xR,x));
+            if error > opt.tol
+                warning(['Variables out of bounds, Violation norm1 = ',num2str(error)])
+            end
         end
     end
 end
