3b2ad1634b45ad0914e4664a38c6f374ccc426f9
Andres Codas <andrescodas@itk.ntnu.no>
2015-02-05 10:31:58 +0100

    bugFix: deal with extra arguments in recursive arroba

--- optimization/utils/callArroba.m
+++ optimization/utils/callArroba.m
@@ -10,24 +10,32 @@ function [ varargout ] = callArroba(fun,args,varargin)
 
 if isstruct(fun)
     assert( ((nargin <= 2) || fun.allowAdditionalArgs) )
+    nDefaultArgs = numel(fun.argsOrder);
+    assert( (numel(args) <= nDefaultArgs) | (fun.allowAdditionalArgs)  ) %% ups... you gave more arguments than needed!
     
-    builtArgs = fun.fixedArgs;
+    nBuiltArgs = sum(fun.argsOrder>0) + numel(fun.fixedArgs);
+    builtArgs = cell(1,nBuiltArgs);
+    remaingArgsIndex = 1:nBuiltArgs;    
     
-    for k = 1:numel(fun.argsOrder)
+    for k = 1:nDefaultArgs
         if fun.argsOrder(k)>0  %% only positive indexes are considered
-            builtArgs = [builtArgs(1:(fun.argsOrder(k)-1)),...
-                args{k},...
-                builtArgs(fun.argsOrder(k):end)];
+            builtArgs{fun.argsOrder(k)} = args{k};
+            remaingArgsIndex = setdiff(remaingArgsIndex,fun.argsOrder(k));
         end
+    end
+    for k = 1:numel(remaingArgsIndex)
+        builtArgs{remaingArgsIndex(k)} = fun.fixedArgs{k};
     end
     
     
     varargout = cell(1:nargout);
     
+    remainingArgs = args(nDefaultArgs+1:end);
+    
     if isstruct(fun.f)  %% allow recursive calling of arroba functions
-        [varargout{1:nargout}] = callArroba(fun.f,builtArgs,varargin{:});    
+        [varargout{1:nargout}] = callArroba(fun.f,builtArgs,remainingArgs{:},varargin{:});    
     else
-        [varargout{1:nargout}] = fun.f(builtArgs{:},varargin{:});
+        [varargout{1:nargout}] = fun.f(builtArgs{:},remainingArgs{:},varargin{:});
     end
     
 else  %% should be a function handle!
