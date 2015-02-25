0df30ad9d58ecb12f56b199cd0640a3e5bd60af5
Andres Codas <andrescodas@itk.ntnu.no>
2015-02-25 22:05:34 +0100

    delete getCellStatuts, use getCellStatutsVO in mrst

--- mrstLink/wrappers/procedural/stateMrst2statePOG.m
+++ mrstLink/wrappers/procedural/stateMrst2statePOG.m
@@ -15,26 +15,27 @@ rs   = state.rs;
 rv   = state.rv;
 
 %Initialization of primary variables ----------------------------------
-[st1 , st2  , st3 ] = getCellStatus(state , disgas, vapoil);
+st  = getCellStatusVO(state,  1-sW-sG,   sW,  sG,  disgas, vapoil);
 
 % define primary varible x and initialize
-x = st1.*rs + st2.*rv + st3.*sG;
+x = st{1}.*rs + st{2}.*rv + st{3}.*sG;
 
 if opt.partials
     [p, sW, x] = initVariablesADI(p, sW, x);
 end
 
+% calculateHydrocarbonsFromStatus
 % define sG, rs and rv in terms of x
-sG = st2.*(1-sW) + st3.*x;
+sG = st{2}.*(1-sW) + st{3}.*x;
 if disgas
     rsSat = f.rsSat(p);
-    rs = (~st1).*rsSat + st1.*x;
+    rs = (~st{1}).*rsSat + st{1}.*x;
 else % otherwise rs = rsSat = const
     %rsSat = rs;
 end
 if vapoil
     rvSat = f.rvSat(p);
-    rv = (~st2).*rvSat + st2.*x;
+    rv = (~st{2}).*rvSat + st{2}.*x;
 else % otherwise rv = rvSat = const
     %rvSat = rv;
 end
@@ -49,7 +50,8 @@ end
 
 % OIL PROPS
 if disgas
-    bO  = f.bO(p, rs, false(size(double(p))));
+    %bO  = f.bO(p, rs, false(size(double(p))));?
+	bO  = f.bO(p,  rs, ~st{1});
 else
     bO  = f.bO(p);
 end
@@ -57,7 +59,8 @@ end
 
 % GAS PROPS (calculated at oil pressure)
 if vapoil
-    bG  = f.bG(p, rv, ~st2);
+    %bG  = f.bG(p, rv, false(size(double(p))));?
+	bG  = f.bG(p, rv, ~st{2});
 else
     bG  = f.bG(p);
 end
