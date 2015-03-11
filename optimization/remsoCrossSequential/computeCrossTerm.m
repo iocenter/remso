dcd53c86460369807c157170d2bce1eac95d3f82
Andres Codas <andrescodas@itk.ntnu.no>
2015-03-11 18:54:27 +0100

    shorten step size if simulation did not converge in crossterm

--- optimization/remsoCrossSequential/computeCrossTerm.m
+++ optimization/remsoCrossSequential/computeCrossTerm.m
@@ -1,22 +1,26 @@
 function [ w,stepY ] = computeCrossTerm(x,u,v,ax,av,gbarZ,ss,obj,mudx,mudu,mudv,lbx,lbv,ubx,ubv,withAlgs,varargin)
 
-opt = struct('xs',[],'vs',[]);
+opt = struct('xs',[],'vs',[],'minStep',1e-6);
 opt = merge_options(opt, varargin{:});
 
-    [stepY] = maximumStepLength([x;v],[ax;av],[lbx;lbv],[ubx;ubv],'tol',0);
-    if stepY == 0
-        axN = cellfun(@(x)-1*x,ax,'UniformOutput',false);
-        if withAlgs
-            avN = cellfun(@(x)-1*x,av,'UniformOutput',false);
-        end
-        [stepY] = maximumStepLength([x;v],[axN;avN],[lbx;lbv],[ubx;ubv],'tol',0);
-        stepY = -stepY;
+[stepY] = maximumStepLength([x;v],[ax;av],[lbx;lbv],[ubx;ubv],'tol',0);
+if stepY == 0
+    axN = cellfun(@(x)-1*x,ax,'UniformOutput',false);
+    if withAlgs
+        avN = cellfun(@(x)-1*x,av,'UniformOutput',false);
     end
-    if stepY == 0
-        % Impossible to compute the correction step by finite differences
-        w = cellfun(@(xx)zeros([size(xx,2),size(xx,1)]),u','UniformOutput',false);
-    else
-
+    [stepY] = maximumStepLength([x;v],[axN;avN],[lbx;lbv],[ubx;ubv],'tol',0);
+    stepY = -stepY;
+end
+if stepY == 0
+    % Impossible to compute the correction step by finite differences
+    w = cellfun(@(xx)zeros([size(xx,2),size(xx,1)]),u','UniformOutput',false);
+else
+    convergedR = false;
+    
+    while ~all(convergedR) && abs(stepY) > opt.minStep
+        
+        
         if stepY == 1
             xR = cellfun(@(z,dz)z+dz,x,ax,'UniformOutput',false);
             vR = cellfun(@(z,dz)z+dz,v,av,'UniformOutput',false);
@@ -33,15 +37,27 @@ opt = merge_options(opt, varargin{:});
             if ~isempty(opt.xs)
                 xRG = cellfun(@(x1,x2,x3)x1*(1-stepY)+(x2+x3)*stepY,opt.xs,x,ax,'UniformOutput',false);
             end
-
+            
             if ~isempty(opt.vs)
                 vRG = cellfun(@(x1,x2,x3)x1*(1-stepY)+(x2+x3)*stepY,opt.vs,v,av,'UniformOutput',false);
-            end            
-
+            end
+            
+        end
+        try
+            [xsR,vsR,~,convergedR,simVarsR,uslicedR] = simulateSystem(xR,u,ss,'gradients',false,'guessX',xRG,'guessV',vRG,'withAlgs',withAlgs);
+            if ~all(convergedR)
+                stepY = stepY/2;
+            end
+        catch
+            warning(['Simulation fails when computing cross-term. Reducing step= ' num2str(stepY)])
+            convergedR = false;
+            stepY = stepY/2;
         end
         
-        [xsR,vsR,~,convergedR,simVarsR,uslicedR] = simulateSystem(xR,u,ss,'gradients',false,'guessX',xRG,'guessV',vRG,'withAlgs',withAlgs);
-
+        
+    end
+    if all(convergedR)
+        
         xdR = cellfun(@(xsi,xi)xsi-xi,xsR,xR,'UniformOutput',false);
         vdR = cellfun(@(vsi,vi)vsi-vi,vsR,vR,'UniformOutput',false);
         
@@ -60,8 +76,14 @@ opt = merge_options(opt, varargin{:});
         else
             w = cellfun(@(x1,x2)(1/stepY)*(x1-x2),gradUY,gbarZ,'UniformOutput',false);
         end
+    else
+        % This is very bad, lets see if we can continue without the
+        % cross term
+        w = cellfun(@(xx)zeros([size(xx,2),size(xx,1)]),u','UniformOutput',false);
+        stepY = 0;
     end
-    
+end
+
 
 
 end
