89d62da6eeaf172c592bfa42a15765aa9a9e25d1
Andres Codas <andrescodas@itk.ntnu.no>
2015-04-10 13:58:22 +0200

    re-implemented assume sparse matrix

--- optimization/utils/cell2matFill.m
+++ optimization/utils/cell2matFill.m
@@ -3,25 +3,37 @@ function [ matrix ] = cell2matFill( cellMatrix,iDims,jDims)
 %
 %
 
-[di,dj] = size(cellMatrix);
-
 if nargin < 2
+    [di,dj] = size(cellMatrix);
     celldim = size(cellMatrix{1,1});
     iDims = celldim(1)*ones(di,1);
     jDims = celldim(2)*ones(1,dj);
 end
 
-matrix = zeros(sum(iDims),sum(jDims));
-
-
-for i = 1:di
-    for j = 1:dj
-        if ~isempty(cellMatrix{i,j})
-            matrix(sum([1;iDims(1:i-1)]):sum([0;iDims(1:i)]),sum([1,jDims(1:j-1)]):sum([0,jDims(1:j)])) = cellMatrix{i,j};
-        end
-    end
+if isrow(iDims)
+    iDims = iDims';
 end
-matrix = sparse(matrix);
+cumI = cumsum([0;iDims(1:end-1)]);
+
+if iscolumn(jDims)
+    jDims = jDims';
+end 
+cumJ = cumsum([0,jDims(1:end-1)]);
+   
+cumI = repmat(num2cell(cumI),1,numel(jDims));
+cumJ = repmat(num2cell(cumJ),numel(iDims),1);
+
+[cellMatrixI,cellMatrixJ,cellMatrixV] = cellfun(@find,cellMatrix,'UniformOutput',false);
+
+cellMatrixI = cellfun(@(ciM,cic)ciM+cic,cellMatrixI,cumI,'UniformOutput',false);
+cellMatrixJ = cellfun(@(ciM,cic)ciM+cic,cellMatrixJ,cumJ,'UniformOutput',false);
+
+
+i = vertcat(cellMatrixI{:});
+j = vertcat(cellMatrixJ{:});
+v = vertcat(cellMatrixV{:});
+
+matrix = sparse(i,j,v,sum(iDims),sum(jDims));
 
 
 
