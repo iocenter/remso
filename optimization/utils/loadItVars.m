d2503cc24f0059a1916b62f6548ea59793347013
Andres Codas <andrescodas@itk.ntnu.no>
2015-05-28 11:55:49 +0200

    bugfix

--- optimization/utils/loadItVars.m
+++ optimization/utils/loadItVars.m
@@ -7,12 +7,13 @@ opt = struct('dir','./','name','itVars','it',0,'r',0);
 opt = merge_options(opt, varargin{:});
 
 name = fullfile(opt.dir,opt.name);
-if opt.it > 0
-    name = [name,'_',num2str(opt.it)];
-end
 if opt.r > 0
-    name = [name,'_',num2str(opt.r)];
+    name = [name,'_r',num2str(opt.r)];
 end
+if opt.it > 0
+    name = [name,'_it',num2str(opt.it)];
+end
+
 
 loadVars = load(name,'u','x','xs','v','vs','simVars');
 u = loadVars.u;
