5d89dedbc034eebe46a41b94c00096782bec9173
Andres Codas <andrescodas@itk.ntnu.no>
2015-06-29 11:53:13 +0200

    remove unnecessary for loop

--- optimization/testFunctions/testMPI/checkSendReceive.m
+++ optimization/testFunctions/testMPI/checkSendReceive.m
@@ -3,36 +3,32 @@ function [ ok,callCount,k] = checkSendReceive(nRanks)
 %   Detailed explanation goes here
 
 ok = true;
-
-varsSend = cell(nRanks,1);
-varsRecv = cell(nRanks,1);
+k = 0;
 
 
-for i = 1:nRanks
-    for j = setdiff(1:nRanks,i)
+for i = 0:nRanks-1
+    for j = setdiff(0:nRanks-1,i)
         
         callCount = 1;
         
         while exist([num2str(i),'_',num2str(j),'_',num2str(callCount),'_Send_MPI.mat']) && ok
-            for k = 0:nRanks-1
+                k = k+1;
                 loadmat = load([num2str(i),'_',num2str(j),'_',num2str(callCount),'_Send_MPI']);
                 mat = loadmat.mat;
-                varsSend{k+1} = mat;
+                varsSend = mat;
                 
                 loadmat = load([num2str(i),'_',num2str(j),'_',num2str(callCount),'_Recv_MPI']);
                 mat = loadmat.mat;
-                varsRecv{k+1} = mat;
-            end
-                    
-            n = numel(varsSend{1});
-            masterVal = reshape(varsSend{1},n,1) ;
+                varsRecv = mat;
             
-            for k = 1:nRanks
-                ok = ok & all(masterVal == reshape(varsRecv{k},n,1));
+                    
+            n = numel(varsSend);
+            masterVal = reshape(varsSend,n,1) ;
+            
+                ok = ok & all(masterVal == reshape(varsRecv,n,1));
                 if ~ok
                     break;
                 end
-            end
             
             callCount = callCount +1 ;
         end
