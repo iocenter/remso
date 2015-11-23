b0b301c22a5df26b505d2abd4707e1dd6e3079bf
thiago <remso@beehive.itk.ntnu.no>
2015-11-23 19:32:53 +0100

    changes in the model of ESPs

--- netLink/networkFunctions/pump_dp_cst.m
+++ netLink/networkFunctions/pump_dp_cst.m
@@ -1,4 +1,4 @@
-function [ dp ] = pump_dp_cst(qp)
+function [ dp ] = pump_dp_cst(qp, rho)
 % Calculates the pressure loss in the pump for a given flow.
 % It can be used to compute upper and lower bounds for the pressure loss
 % in the equipment. The coefficients of the curve were found in the paper
@@ -6,13 +6,8 @@ function [ dp ] = pump_dp_cst(qp)
 % gathering networks with esp-produced, high water cut wells'.
 %    
 
-    a0 = 19.37;       % m 
-    a1 = -1.23e-02;     % in m/sm3/d
-    a2 = 2.24e-05;      % in m/(sm3/d)^2      
-    a3 = -1.86e-08;     % in m/(sm3/d)^3
-    a4 = 4.13e-12;      % in m/(sm3/d)^4
+    dh = pump_dh(qp); % in m    
     
-    dp = a4.*qp.^4 + a3.*qp.^3 + a2.*qp.^2 + a1.*qp + a0;
-
+    dp = rho.*norm(gravity).*dh;
 end
 
