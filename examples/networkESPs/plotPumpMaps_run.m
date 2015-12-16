6a6f22294de30ddaf9468dc38e8c159a6d362975
thiago <remso@beehive.itk.ntnu.no>
2015-12-17 00:15:00 +0100

    bug fixes in the plotting of the pumps

--- examples/networkESPs/plotPumpMaps_run.m
+++ examples/networkESPs/plotPumpMaps_run.m
@@ -1,15 +1,15 @@
 % qf_start = 636*(meter^3/day);
 % qf_end   = 1542.*(meter^3/day);
 
-qf_start = 300*(meter^3/day);
-qf_end   = 1842.*(meter^3/day);
+qf_start = 1*(meter^3/day);
+qf_end   = 500.*(meter^3/day);
 numFlows = 50;  
 
 freq_start = 30;
 freq_end   = 60;
 numFreq  = 10;
 
-numStages = 100;
+numStages = 60;
 fref = 60;
 
 plotPumpDh(qf_start, qf_end, numFlows, freq_start, freq_end, numFreq, numStages, fref);
