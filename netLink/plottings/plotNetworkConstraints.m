b0774ca5c5325ed6da762f5f575a76cda0ede3c8
thiago <remso@beehive.itk.ntnu.no>
2015-12-17 17:59:17 +0100

    comments and todo

--- netLink/plottings/plotNetworkConstraints.m
+++ netLink/plottings/plotNetworkConstraints.m
@@ -1,5 +1,8 @@
 function [figN] =  plotNetworkConstraints(v, lbv, ubv, nScale, times, netCst, figN)
-   
+%%plotNetworkConstraints function that provide plots for network
+%%constraints.
+
+%% TODO: should receive size of each type of constraint, split the vector v, lbv, ubv, and call corresponding functions
     dp = zeros(netCst, numel(v));   
     ldp = zeros(netCst, numel(v));   
     udp  = zeros(netCst, numel(v));   
@@ -18,13 +21,7 @@ function [figN] =  plotNetworkConstraints(v, lbv, ubv, nScale, times, netCst, fi
         
         plot(times.steps(2:end), dp(i,:), 'kx-', times.steps(2:end), ldp(i,:), 'rv-',times.steps(2:end), udp(i,:), 'r^-' );
         
-%         plot(times.steps(2:end), ldp(i,:), 'rv-');
-        
         plot(times.steps(2:end), dp(i,:), 'kx-', times.steps(2:end), ldp(i,:), 'rv-',times.steps(2:end), udp(i,:), 'r^-' );
-        
-%         plot(times.steps(2:end), ldp(i,:), 'rv-');
-        
-%         plot(times.steps(2:end), udp(i,:), 'r^-');
 
         title(strcat('Well: ', int2str(i)));       
         
@@ -32,4 +29,3 @@ function [figN] =  plotNetworkConstraints(v, lbv, ubv, nScale, times, netCst, fi
         ylabel('frequency (Hertz)');
     end
 end
-
