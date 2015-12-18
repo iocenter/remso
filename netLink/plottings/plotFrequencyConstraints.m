f8a22f39ac7f741a4969bea73875d3aca58d4ba0
thiago <remso@beehive.itk.ntnu.no>
2015-12-18 17:43:17 +0100

    improvements in plot functionalities

--- netLink/plottings/plotFrequencyConstraints.m
+++ netLink/plottings/plotFrequencyConstraints.m
@@ -1,24 +1,13 @@
-function [figN] =  plotFrequencyConstraints(v, lbv, ubv, nScale, times, netCst, figN)
-   
-    dp = zeros(netCst, numel(v));   
-    ldp = zeros(netCst, numel(v));   
-    udp  = zeros(netCst, numel(v));   
-    
+function [figN] =  plotFrequencyConstraints(freq, lfreq, ufreq, times, netCst, figN)
+%%plotFrequencyConstraints plots constraints on the frequency of pumps
+%%(ESPs) of the network    
     for i=1:netCst
         figure(figN); 
         figN = figN+1;
         
-        for j=1:numel(v)
-            dp(i,j) = v{j}(end-netCst+i-1).*nScale(i);
-            
-            ldp(i,j) = lbv{j}(end-netCst+i-1).*nScale(i);            
-            udp(i,j) = ubv{j}(end-netCst+i-1).*nScale(i);
-        end
-               
+        plot(times.steps(2:end), freq(i,:), 'kx-', times.steps(2:end), lfreq(i,:), 'rv-',times.steps(2:end), ufreq(i,:), 'r^-' );
         
-        plot(times.steps(2:end), dp(i,:), 'kx-', times.steps(2:end), ldp(i,:), 'rv-',times.steps(2:end), udp(i,:), 'r^-' );
-        
-        plot(times.steps(2:end), dp(i,:), 'kx-', times.steps(2:end), ldp(i,:), 'rv-',times.steps(2:end), udp(i,:), 'r^-' );
+        plot(times.steps(2:end), freq(i,:), 'kx-', times.steps(2:end), lfreq(i,:), 'rv-',times.steps(2:end), ufreq(i,:), 'r^-' );
 
         title(strcat('Well: ', int2str(i)));       
         
--- netLink/plottings/plotNetworkConstraints.m
+++ netLink/plottings/plotNetworkConstraints.m
@@ -1,31 +1,35 @@
-function [figN] =  plotNetworkConstraints(v, lbv, ubv, nScale, times, netCst, figN)
+function [figN] =  plotNetworkConstraints(v, lbv, ubv, nScale, times, netCst, figN, varargin)
 %%plotNetworkConstraints function that provide plots for network
 %%constraints.
+    opt     = struct('freqCst', 0, 'pressureCst', 0, 'flowCst', 0);
+    opt     = merge_options(opt, varargin{:});
+
 
 %% TODO: should receive size of each type of constraint, split the vector v, lbv, ubv, and call corresponding functions
     dp = zeros(netCst, numel(v));   
     ldp = zeros(netCst, numel(v));   
     udp  = zeros(netCst, numel(v));   
     
+    
     for i=1:netCst
-        figure(figN); 
-        figN = figN+1;
-        
         for j=1:numel(v)
             dp(i,j) = v{j}(end-netCst+i-1).*nScale(i);
             
             ldp(i,j) = lbv{j}(end-netCst+i-1).*nScale(i);            
             udp(i,j) = ubv{j}(end-netCst+i-1).*nScale(i);
         end
-               
-        
-        plot(times.steps(2:end), dp(i,:), 'kx-', times.steps(2:end), ldp(i,:), 'rv-',times.steps(2:end), udp(i,:), 'r^-' );
-        
-        plot(times.steps(2:end), dp(i,:), 'kx-', times.steps(2:end), ldp(i,:), 'rv-',times.steps(2:end), udp(i,:), 'r^-' );
-
-        title(strcat('Well: ', int2str(i)));       
-        
-        xlabel('time (days)');
-        ylabel('frequency (Hertz)');
     end
+            
+    if opt.freqCst>0
+        figN = plotFrequencyConstraints(dp(1:opt.freqCst, :), ldp(1:opt.freqCst, :), udp(1:opt.freqCst, :), times, opt.freqCst, figN);
+    end
+    
+    if opt.pressureCst>0
+        figN = plotPressureConstraints(dp(opt.freqCst+1:opt.freqCst+opt.pressureCst, :), ldp(opt.freqCst+1:opt.freqCst+opt.pressureCst, :), udp(opt.freqCst+1:opt.freqCst+opt.pressureCst, :), times, opt.pressureCst, figN);
+    end
+    
+    if opt.flowCst>0      
+        figN = plotFlowConstraints(dp(opt.freqCst+opt.pressureCst+1:end, :), ldp(opt.freqCst+opt.pressureCst+1:end, :), udp(opt.freqCst+opt.pressureCst+1:end, :), times, opt.flowCst, figN);
+    end
+ 
 end
