ce97599723450cc8050f8c40b9dd29743c925fb6
thiago <remso@beehive.itk.ntnu.no>
2016-01-13 17:16:30 +0100

    included pump equations in the model
    Frequencies of the pumps are calculated for given pressure differences
    (that gives head difference depending on the density of the fluids).

---
+++ netLink/networkFunctions/pump_dh.m
@@ -0,0 +1,12 @@
+function [ dhf ] = pump_dh(dpf, rho)
+% Calculates the head difference for the pump at a given pressure loss.
+% It can be used to compute upper and lower bounds for the pressure loss
+% in the equipment. The coefficients of the curve were found in the paper
+% 'Exploring the potential of model-based optimization in oil production
+% gathering networks with esp-produced, high water cut wells'.
+%    
+    dhf = dpf./(rho.*norm(gravity));
+end
+
+
+
