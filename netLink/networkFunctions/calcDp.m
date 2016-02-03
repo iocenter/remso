d2e6d67869bde2e4d46d6e72e8991f8ad70c6eec
thiago <remso@beehive.itk.ntnu.no>
2016-02-03 20:14:42 +0100

    mixture density for the pump map plot is not fixed anymore.

--- netLink/networkFunctions/calcDp.m
+++ netLink/networkFunctions/calcDp.m
@@ -1,6 +1,8 @@
-function dp = calcDp(q, f, fref, nStages)        
-    dh  = pumpDhExplicit(q).*((f./fref).^2).*nStages;
-    mixDens = 900;
-    dp  = (dh.*norm(gravity).*mixDens)./barsa;
-
+function dp = calcDp(q, f, fref, nStages, varargin)        
+    opt     = struct('mixDensity', 900);
+    opt     = merge_options(opt, varargin{:});
+    
+    
+    dh  = pumpDhExplicit(q).*((f./fref).^2).*nStages;    
+    dp  = (dh.*norm(gravity).*opt.mixDensity)./barsa;
 end
