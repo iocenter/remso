ba163f73403fae5d0b9774a3337ce675191d976f
thiago <remso@beehive.itk.ntnu.no>
2016-02-03 15:59:42 +0100

    bug fix: vectorized calculation

--- netLink/networkFunctions/calcDp.m
+++ netLink/networkFunctions/calcDp.m
@@ -1,6 +1,6 @@
 function dp = calcDp(q, f, fref, nStages)        
-    dh  = pumpDhExplicit(q)*((f./fref).^2)*nStages;
+    dh  = pumpDhExplicit(q).*((f./fref).^2).*nStages;
     mixDens = 900;
-    dp  = (dh*norm(gravity)*mixDens)./barsa;
+    dp  = (dh.*norm(gravity).*mixDens)./barsa;
 
 end
