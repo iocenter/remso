3b233156f67541047b6b311ea77ff021674a063d
thiago <remso@beehive.itk.ntnu.no>
2016-03-29 19:50:17 +0200

    inclusion of roughness in the pipeline description

--- netLink/networkFunctions/newPipeline.m
+++ netLink/networkFunctions/newPipeline.m
@@ -5,7 +5,7 @@ function pipe = newPipeline(varargin)
         error('Missing Parameters to invoke function newPipeline ');
     end
 
-   opt = struct('diam',[],'len',[],'ang',[],'temp',[]);
+   opt = struct('diam',[],'len',[],'ang',[],'temp',[], 'roughness', 2.8*10^-5*meter);
    pipe = merge_options(opt, varargin{:});
 end
 
