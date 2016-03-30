5db78305ae9c7a0c5bb1d4e113c181cf8ede4e26
Andres Codas <andrescodas@itk.ntnu.no>
2016-03-30 13:01:41 +0200

    make function compatible with ADI

--- netLink/fluidProperties/gas_oil_rat_Standing.m
+++ netLink/fluidProperties/gas_oil_rat_Standing.m
@@ -10,6 +10,9 @@ function R_s = gas_oil_rat_Standing(p,rho_g_sc,rho_o_sc,T)
 % T = temperature, deg. C
 %
 % JDJ, 27-02-01, last revised 10-05-13
-
-help01 = 10^(1768/rho_o_sc - 0.00164*T);
-R_s = (rho_g_sc/716)*((8e-6*p+1.4)*help01)^1.2048;
+%{
+Changes Codas
+Make the function compatible with ADI objects
+%}
+help01 = 10.^(1768./rho_o_sc - 0.00164*T);
+R_s = (rho_g_sc./716)*((8e-6*p+1.4)*help01).^1.2048;
