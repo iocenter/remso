b39d682bc6a8c0cb594d5949067f26cbef66e946
thiago <remso@beehive.itk.ntnu.no>
2016-04-01 14:58:27 +0200

    improvements in plotting for reservoir

--- netLink/auxiliaryFunctions/plotReservoir.m
+++ netLink/auxiliaryFunctions/plotReservoir.m
@@ -38,10 +38,32 @@ function [] = plotReservoir(reservoirP, W, varargin)
         if opt.showPressures
             plotCellData(G, states{i}.pressure./barsa) % pressure
         end
-
+        
         if opt.showSaturations
             plotCellData(G, states{i}.s(:,2)) % saturation
         end
         pause(0.01);
     end
+    
+    
+    
+    [i j k] = ind2sub(G.cartDims, 1:G.cells.num);
+    clf;
+    plotGrid(G, 'FaceAlpha', 0, 'EdgeAlpha', .1)
+%     plotCellData(G, reservoirP.state.pressure./barsa, j == round(G.cartDims(2)/2))
+    plotCellData(G, reservoirP.state.pressure./barsa);
+    % Plot the wells
+    plotWell(G, W);
+    view(30,50)
+%     colorbar;
+    
+    [i j k] = ind2sub(G.cartDims, 1:G.cells.num);
+    clf;
+    plotGrid(G, 'FaceAlpha', 0, 'EdgeAlpha', .1)
+    plotCellData(G, states{50}.pressure./barsa, j == round(G.cartDims(2)/2))
+    % Plot the wells
+    plotWell(G, W);
+    view(30,50)
+    
+    
 end
