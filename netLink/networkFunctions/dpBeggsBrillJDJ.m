a565ba139f204b1defea7994dec4185374db4454
Andres Codas <andrescodas@itk.ntnu.no>
2016-04-08 18:42:18 +0200

    update interface

--- netLink/networkFunctions/dpBeggsBrillJDJ.m
+++ netLink/networkFunctions/dpBeggsBrillJDJ.m
@@ -1,4 +1,4 @@
-function [dpds_tot,f_work,Z] =  dpBeggsBrillJDJ(E, qoE, qwE, qgE, pV,f_work,Z)
+function [dpds_tot,f_work,Z] =  dpBeggsBrillJDJ(E, qoE, qwE, qgE, pV,hasSurfaceGas,f_work,Z)
 %% dpBeggsBrillJDJ - wrapper to Beggs_Brill_dpds
 %% dp: calculates the pressure drop for the average pressure pV
 %%     the functions is implemented for SI units (input).
@@ -11,15 +11,17 @@ s = 0;
 if any((E.pipeline.ang > pi/2)) || any((E.pipeline.ang < -pi/2))
     warning('check angle definition')
 end
-if nargin < 6
+if nargin < 7
     f_work = [];
     Z = [];
+end
+if nargin < 6
+    hasSurfaceGas = true;
 end
 alpha = pi/2-E.pipeline.ang;
 d = E.pipeline.diam;
 e = E.pipeline.roughness;
 oil = 1; % black oil; parameters computed with the aid of Standing correlations
-q_sc = [qgE; qoE; qwE]; % in sm^3/s  
 rho_sc = [E.stream.gas_dens,E.stream.oil_dens,E.stream.water_dens]; % in kg/m^3 
 s_in = 0; 
 s_out = 1; 
@@ -27,6 +29,6 @@ T = E.pipeline.temp - 273.15;
 T_in = T;
 T_out =T;
 
-[dpVector,f_work,Z] = Beggs_Brill_dpds(s,pV,[],alpha,d,e,oil,q_sc,rho_sc,s_in,s_out,T_in,T_out,hasSurfaceGas,f_work,Z);
+[dpVector,f_work,Z] = Beggs_Brill_dpds(s,pV,[],alpha,d,e,oil,qgE, qoE, qwE,       rho_sc,s_in,s_out,T_in,T_out,hasSurfaceGas,f_work,Z);
 dpds_tot = dpVector(1);
 end
