5fb10eea210b8c27fbaf6d0e7d0d128099939843
Andres Codas <andrescodas@itk.ntnu.no>
2016-04-08 18:21:50 +0200

    improve parameters and methods for the efficiency of cvodes

--- netLink/networkFunctions/dpCVODES.m
+++ netLink/networkFunctions/dpCVODES.m
@@ -5,7 +5,7 @@ function [ dpTotal ] = dpCVODES(Eout,  qo, qw, qg, p, varargin)
 
 opt     = struct('dpFunction', @simpleDp, ...
     'monitor',false,...
-    'forwardGradient',false,...
+    'forwardGradient',true,...
     'pScale', 5*barsa,...
     'qlScale', 5*meter^3/day,...
     'qgScale', 100*(10*ft)^3/day);
@@ -32,17 +32,20 @@ data.fixedParameters = fixedParameters;
 data.variableParameters = variableParameters;
 data.paramScaling = paramScaling;
 data.outputScaling = outputScaling;
+data.p = nan;
+data.dp = nan;
+
 
 % ---------------------
 % CVODES initialization
 % ---------------------
 %% This is way too precise, consider relaxing in the actual application
-InitialStep = 5;
+InitialStep = integrationSteps;
 options = CVodeSetOptions('UserData',data,...
     'RelTol',1.e-4,...
-    'AbsTol',1.e-8,...
+    'AbsTol',1.e-2,...
     'JacobianFn',@djacfn,...
-    'MaxStep',integrationSteps,...
+    'MaxStep',inf,...
     'InitialStep',InitialStep,...
     'SensDependent',true);
 
@@ -60,7 +63,7 @@ xf = pipeSizes;
 
 
 p0 = p;
-CVodeInit(@rhsfn, 'BDF', 'Newton', x0, double(p0)/outputScaling, options);
+CVodeInit(@rhsfn, 'Adams' , 'Functional', x0, double(p0)/outputScaling, options);
 
 if computeGradients && ~opt.forwardGradient
     CVodeAdjInit(150, 'Hermite');  %% TODO: study how to tune the magic number 150
@@ -76,7 +79,7 @@ if computeGradients && opt.forwardGradient
     yS0 = [0,0,0,1];
     
     
-    FSAoptions = CVodeSensSetOptions('method','Simultaneous','ErrControl', true); 
+    FSAoptions = CVodeSensSetOptions('method','Staggered','ErrControl', false); 
     CVodeSensInit(Ns, @rhsSfn, yS0, FSAoptions);
     
     
@@ -106,8 +109,10 @@ if computeGradients && ~opt.forwardGradient
     
     %%TODO: check how to add 'SensDependent',true
     optionsB = CVodeSetOptions('UserData',data,...
-        'RelTol',1.e-4,...
-        'AbsTol',1.e-8,...
+            'RelTol',1.e-4,...
+            'AbsTol',1.e-2,...
+            'MaxStep',inf,...
+            'InitialStep',-InitialStep,...%'SensDependent',true,...
         'JacobianFn',@djacBfn);
     
     if opt.monitor
@@ -119,10 +124,10 @@ if computeGradients && ~opt.forwardGradient
     end
     
     
-    idxB = CVodeInitB(@rhsBfn, 'BDF', 'Newton', xf, 1, optionsB);
+    idxB = CVodeInitB(@rhsBfn, 'Adams' , 'Functional', xf, 1, optionsB);
     
-    optionsQB = CVodeQuadSetOptions('ErrControl',true,...
-        'RelTol',1.e-4,'AbsTol',1.e-8);
+    optionsQB = CVodeQuadSetOptions('ErrControl',false,...
+        'RelTol',1.e-4,'AbsTol',1.e-2);
     
     CVodeQuadInitB(idxB, @quadBfn, [0;0;0], optionsQB);
     
@@ -143,7 +148,7 @@ if computeGradients && ~opt.forwardGradient
                          )*outputScaling...
                          ,1, cellfun(@(x)size(x,2),qo.jac) );
     pFinal = ADI(pFinal,jacPfinal);
-    
+
 end
 
 
@@ -169,13 +174,18 @@ end
 
 function [dp, flag, new_data] = rhsfn(x, p, data)
 
-
-dp = data.dpFunction(data.fixedParameters{:},data.variableParameters{:},p*data.outputScaling)/data.outputScaling;
-
+if p == data.p
+    dp = double(data.dp);
+    new_data = [];
+else
+    dp = data.dpFunction(data.fixedParameters{:},data.variableParameters{:},p*data.outputScaling)/data.outputScaling;
+    data.p=p;
+    data.dp=dp;
+    new_data = data;
+end
 
 
 flag = 0;
-new_data = [];
 
 end
 
@@ -185,21 +195,22 @@ function [J, flag, new_data] = djacfn(x, p, fp, data)
 % Dense Jacobian function
 
 
-[p] = initVariablesADI(p);
+[qo, qw, qg,p] = initVariablesADI(data.variableParameters{:},p);
 
-dp = data.dpFunction(data.fixedParameters{:},data.variableParameters{:},p*data.outputScaling)/data.outputScaling;
+dp = data.dpFunction(data.fixedParameters{:},qo, qw, qg ,p*data.outputScaling)/data.outputScaling;
 
 %assert(fp == double(dp));
 
 if isa(dp,'ADI')
-    J = dp.jac{1};
+    J = dp.jac{4};
 else
     J = 0;  %% dp does not depend on p
 end
-
+data.p = double(p);
+data.dp = dp;
 
 flag = 0;
-new_data = [];
+new_data = data;
 
 end
 
@@ -240,15 +251,22 @@ end
 function [qBd, flag, new_data] = quadBfn(x, p, lambda, data)
 % Backward problem quadrature integrand function
 
+if data.p == p && isa(data.dp,'ADI')
+    
+    dp = data.dp;
+    
+else
 
-[qo, qw, qg] = initVariablesADI(data.variableParameters{:});
+    [qo, qw, qg] = initVariablesADI(data.variableParameters{:});
 
-dp = data.dpFunction(data.fixedParameters{:},qo, qw, qg ,p*data.outputScaling)/data.outputScaling;
+    dp = data.dpFunction(data.fixedParameters{:},qo, qw, qg ,p*data.outputScaling)/data.outputScaling;
+end
 
 qBd = -lambda'*[dp.jac{1}*data.paramScaling{1},...
                 dp.jac{2}*data.paramScaling{2},...
                 dp.jac{3}*data.paramScaling{3}];
 
+            
 flag = 0;
 new_data = [];
 end
@@ -257,10 +275,7 @@ end
 function [JB, flag, new_data] = djacBfn(t, y, yB, fyB, data)
 % Backward problem Jacobian function
 
-J = djacfn(t,y,fyB,data);
+[J,flag,new_data] = djacfn(t,y,fyB,data);
 JB = -J';
-
-flag = 0;
-new_data = [];
 
 end
