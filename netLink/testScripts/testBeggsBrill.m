a5bc746d10bccadf325d4da61bdf1917239cf910
thiago <remso@beehive.itk.ntnu.no>
2016-04-11 16:20:40 +0200

    test with vectorization of gradient calculation with FD

--- netLink/testScripts/testBeggsBrill.m
+++ netLink/testScripts/testBeggsBrill.m
@@ -23,17 +23,16 @@ pBounds = [2*barsa;500*barsa];
 
 q_o_sc = qoBounds(1) + (qoBounds(2)-qoBounds(1))*rand(n,1);
 q_w_sc = qwBounds(1) + (qwBounds(2)-qwBounds(1))*rand(n,1);
-q_g_sc = (q_o_sc*R_sbBounds(1) + q_o_sc.*(R_sbBounds(2)-R_sbBounds(1)).*rand(n,1));
+q_g_sc = (q_o_sc*R_sbBounds(1) + q_o_sc.*(R_sbBounds(2)-R_sbBounds(1)).*rand(n,1))*0;
 pV =  pBounds(1) + ( pBounds(2)- pBounds(1))*rand(n,1);
 
 rho_sc = [0.8000;906.6450;1000];
 
-hasSurfaceGas = true;
+hasSurfaceGas = false;
 
-
-addpath(genpath('/home/codas/Desktop/Transfer/Fluid properties'));
-addpath(genpath('/home/codas/Desktop/Transfer/Pipe flow'));
-addpath(genpath('/home/codas/Desktop/Transfer/Conversion factors'));
+addpath(genpath('/home/thiago/Desktop/Transfer/Fluid properties'));
+addpath(genpath('/home/thiago/Desktop/Transfer/Pipe flow'));
+addpath(genpath('/home/thiago/Desktop/Transfer/Conversion factors'));
 
 
 tic
@@ -75,11 +74,42 @@ qlP = qlScale*pert;
 qgP = qgScale*pert;
 pP =  pScale*pert;
 
-
 gradScale = pScale./[pScale*ones(1,n),qgScale*ones(1,n),qlScale*ones(1,n),qlScale*ones(1,n)];
 
 
 [dpVectorV] = Beggs_Brill_dpds(s,pV,[],alpha,d,e,1,q_g_sc,q_o_sc,q_w_sc,rho_sc,s-1,s+1,T,T,hasSurfaceGas);
+
+
+tic
+if hasSurfaceGas     
+    nPert = 8;
+    pertMatrix = repmat(blkdiag([qgP; -qgP], [qlP; -qlP],[qlP; -qlP], [pP; -pP]), numel(q_g_sc), 1);
+else
+    nPert = 6;
+    pertMatrix = horzcat(zeros(numel(q_o_sc)*nPert,1), repmat(blkdiag([qlP; -qlP],[qlP; -qlP], [pP; -pP]), numel(q_o_sc), 1));    
+end
+
+qFd =  kron([q_g_sc, q_o_sc, q_w_sc, pV], ones(nPert,1)) + pertMatrix;
+TFd =  kron(T, ones(nPert,1));
+alphaFd = kron(alpha, ones(nPert,1));
+dFd =  kron(d, ones(nPert,1));
+eFd = kron(e, ones(nPert,1));
+
+dpPert = reshape(Beggs_Brill_dpds(s,qFd(:,4),[],alphaFd,dFd,eFd,1,qFd(:,1),qFd(:,2),qFd(:,3),rho_sc,s-1,s+1,TFd,TFd,hasSurfaceGas), nPert, numel(q_o_sc))';
+
+if hasSurfaceGas
+    Jgv = (dpPert(:,1)-dpPert(:,2))/(2*qgP);
+    Jov = (dpPert(:,3)-dpPert(:,4))/(2*qlP);
+    Jwv = (dpPert(:,5)-dpPert(:,6))/(2*qlP);
+    Jpv = (dpPert(:,7)-dpPert(:,8))/(2*pP);
+else
+    Jgv = zeros(numel(q_o_sc),1);
+    Jov = (dpPert(:,1)-dpPert(:,2))/(2*qlP);
+    Jwv = (dpPert(:,3)-dpPert(:,4))/(2*qlP);
+    Jpv = (dpPert(:,5)-dpPert(:,6))/(2*pP);
+end
+jacDFVtime = toc
+
 tic
 Jo =  (+Beggs_Brill_dpds(s,pV,[],alpha,d,e,1,q_g_sc,q_o_sc+qlP,q_w_sc,rho_sc,s-1,s+1,T,T,hasSurfaceGas)...
       -Beggs_Brill_dpds(s,pV,[],alpha,d,e,1,q_g_sc,q_o_sc-qlP,q_w_sc,rho_sc,s-1,s+1,T,T,hasSurfaceGas))/(2*qlP);
@@ -87,11 +117,16 @@ Jo =  (+Beggs_Brill_dpds(s,pV,[],alpha,d,e,1,q_g_sc,q_o_sc+qlP,q_w_sc,rho_sc,s-1
 Jw =  (+Beggs_Brill_dpds(s,pV,[],alpha,d,e,1,q_g_sc,q_o_sc,q_w_sc+qlP,rho_sc,s-1,s+1,T,T,hasSurfaceGas)...
       -Beggs_Brill_dpds(s,pV,[],alpha,d,e,1,q_g_sc,q_o_sc,q_w_sc-qlP,rho_sc,s-1,s+1,T,T,hasSurfaceGas))/(2*qlP);
 
-Jg =  (+Beggs_Brill_dpds(s,pV,[],alpha,d,e,1,q_g_sc+qgP,q_o_sc,q_w_sc,rho_sc,s-1,s+1,T,T,hasSurfaceGas)...
-      -Beggs_Brill_dpds(s,pV,[],alpha,d,e,1,q_g_sc-qgP,q_o_sc,q_w_sc,rho_sc,s-1,s+1,T,T,hasSurfaceGas))/(2*qgP);
-  
+if hasSurfaceGas
+    Jg =  (+Beggs_Brill_dpds(s,pV,[],alpha,d,e,1,q_g_sc+qgP,q_o_sc,q_w_sc,rho_sc,s-1,s+1,T,T,hasSurfaceGas)...
+        -Beggs_Brill_dpds(s,pV,[],alpha,d,e,1, max(q_g_sc-qgP,0),q_o_sc,q_w_sc,rho_sc,s-1,s+1,T,T,hasSurfaceGas))/(2*qgP);
+else
+    Jg= zeros(numel(q_o_sc),1);
+end  
+ 
 Jp =  (+Beggs_Brill_dpds(s,pV+pP,[],alpha,d,e,1,q_g_sc,q_o_sc,q_w_sc,rho_sc,s-1,s+1,T,T,hasSurfaceGas)...
-      -Beggs_Brill_dpds(s,pV-pP,[],alpha,d,e,1,q_g_sc,q_o_sc,q_w_sc,rho_sc,s-1,s+1,T,T,hasSurfaceGas))/(2*pP);  
+          -Beggs_Brill_dpds(s,pV-pP,[],alpha,d,e,1,q_g_sc,q_o_sc,q_w_sc,rho_sc,s-1,s+1,T,T,hasSurfaceGas))/(2*pP);  
+
 jacDFtime = toc
 
 
@@ -115,5 +150,7 @@ jacADIT = toc
 %max(max(abs(bsxfun(@rdivide,(cell2mat(dpVectorVAD.jac)-JF),gradScale))))
 max(max(abs(bsxfun(@rdivide,full(cell2mat(dpVectorVAD.jac)-cell2mat(dpVectorVAD2.jac)),gradScale))))
 max(max(abs(bsxfun(@rdivide,(cell2mat(dpVectorVAD.jac)-[diag(Jp),diag(Jg),diag(Jo),diag(Jw)]),gradScale))))
+max(max(bsxfun(@rdivide, [diag(Jpv), diag(Jgv), diag(Jov), diag(Jwv)]-[diag(Jp),diag(Jg),diag(Jo),diag(Jw)], gradScale))) % comparing vectorized FD results against iterative FD
 
+[jacDFtime/jacDFVtime,  jacADIT2/jacDFVtime, jacADIT/jacDFVtime]
 
