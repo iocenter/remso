771e01aad1e197f4e282a6241f67abec12197085
thiago <remso@beehive.itk.ntnu.no>
2016-04-15 13:56:26 +0200

    bugfix: handling mixture of flows (manifolds)

--- netLink/networkFunctions/runNetwork.m
+++ netLink/networkFunctions/runNetwork.m
@@ -38,7 +38,12 @@ ns.pV = forwardDp(ns, Vin, opt.dpFunction, opt.hasGas, opt.forwardGradient, opt.
 
 % Backward propagation of pressures in the network
 surfaceSinks = setdiff(vertcat(ns.Vsnk), vertcat(ns.VwInj));
-Vout = getVertex(ns, surfaceSinks);
+Vout = getVertex(ns, vertcat(surfaceSinks));
+
+%% correction for multiple input pipelines
+Ein = getEdges(ns, Vout.Ein);
+Vout = getVertex(ns, Ein.vout);
+
 ns.pV  = backwardDp(ns,Vout, opt.dpFunction, opt.hasGas, opt.forwardGradient, opt.finiteDiff);
 
 netSol = ns;
@@ -48,8 +53,8 @@ function [pV] = forwardDp(ns, Vin, dpFunction, hasGas, forwardGradient, finiteDi
 % forwardDp: performs foward pressure drop calculations in the network.
 Eout =  getEdge(ns, vertcat(Vin.Eout));   
 condStop = vertcat(Eout.equipment);
-while  ~all(condStop)    
-    [dp, pvK] = vectorizedDp(ns, Eout(~condStop), Vin(~condStop), dpFunction, hasGas, forwardGradient, finiteDiff);    
+while  ~all(condStop)       
+    [dp, pvK] = vectorizedDp(ns, Eout(~condStop), Vin(~condStop), dpFunction, hasGas, forwardGradient, finiteDiff);
   
     idsVout = vertcat(Eout(~condStop).vout);
     ns.pV(idsVout) = pvK - dp;
@@ -66,23 +71,45 @@ end
 pV = ns.pV;
 end
 
+
+function [pV] = propagateDp(ns, Vin, Vout, dpFunction, hasGas, forwardGradient, finiteDiff)
+% forwardDp: performs foward pressure drop calculations in the network.
+Eout =  getEdge(ns, vertcat(Vin.Eout));   
+condStop = vertcat(Eout.equipment);
+while  ~all(condStop)       
+    [dp, pvK] = vectorizedDp(ns, Eout(~condStop), Vin(~condStop), dpFunction, hasGas, forwardGradient, finiteDiff);
+  
+    idsVout = vertcat(Eout(~condStop).vout);
+    ns.pV(idsVout) = pvK - dp;
+    
+    Vin = getVertex(ns, idsVout);
+    Eout = getEdge(ns,  vertcat(Vin.Eout));
+    
+    if numel(Vin) ~= numel(Eout)
+        error('Manifold before the equipment in the network or splitting of flows is happening');
+    end
+    
+    condStop = vertcat(Eout.equipment);     
+end
+pV = ns.pV;
+end
+
+
 function [pV] = backwardDp(ns, Vout, dpFunction, hasGas, forwardGradient, finiteDiff)
 % forwardDp: performs backward pressure drop calculations in the network.
 Ein =  getEdge(ns, vertcat(Vout.Ein));   
 condStop = vertcat(Ein.equipment);
 
-while  ~all(condStop)      
-    [dp, pvK] = vectorizedDp(ns, Ein(~condStop), Vout(~condStop), dpFunction,  hasGas, forwardGradient, finiteDiff);    
+while  ~all(condStop)    
+    voutAux = getVertex(ns, vertcat(Ein(~condStop).vout));   %% to correct size of output vertices  (manifold)    
+    
+    [dp, pvK] = vectorizedDp(ns, Ein(~condStop), voutAux, dpFunction,  hasGas, forwardGradient, finiteDiff);    
   
-    idsVin = vertcat(Ein(~condStop).vin);
+    idsVin = vertcat(Ein(~condStop).vin);    
     ns.pV(idsVin) = pvK + dp;
     
     Vout = getVertex(ns, idsVin);
     Ein = getEdge(ns,  vertcat(Vout.Ein));
-    
-    if numel(Vout) ~= numel(Ein) %% it is a manifold
-        Vout = repmat(Vout, numel(Ein),1);
-    end
     
     condStop = vertcat(Ein.equipment);            
 end
@@ -90,13 +117,13 @@ pV = ns.pV;
 end
 
 function [dp, pvK] = vectorizedDp(ns, E, V, dpFunction, hasGas, forwardGradient, finiteDiff)
-idsEin = vertcat(E.id);    
-idsVout  = vertcat(V.id);
-    
+idsEin = vertcat(E.id);        
+idsVout = vertcat(V.id);
 qoK = ns.qo(idsEin);
 qwK = ns.qw(idsEin);
 qgK = ns.qg(idsEin);
-pvK  = ns.pV(idsVout);
+
+pvK = ns.pV(idsVout);
 
 dp = dpCVODES(E, qoK, qwK, qgK, pvK, 'dpFunction', dpFunction, 'hasSurfaceGas', hasGas,'forwardGradient', forwardGradient, 'finiteDiff', finiteDiff);
 end
