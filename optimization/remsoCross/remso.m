18d3527c81880b38fe48207509590db61e836df6
Andres Codas <andrescodas@itk.ntnu.no>
2016-04-16 14:56:53 +0200

    make sure a descent direction is enforced
    --Required for very tight convergence tolerances

--- optimization/remsoCross/remso.m
+++ optimization/remsoCross/remso.m
@@ -601,6 +601,18 @@ for k = 1:opt.max_iter
         else
             warning('xi == 1. The problem may be infeasible to solve');
         end
+        
+        %% double check the descent condition!
+        objGrad = cell2mat([objPartials.Jx,objPartials.Jv,objPartials.Ju])*cell2mat([dx;dv;du]);
+        errorGrad = (1-xi)*norm(cell2mat([xd;vd]),1);
+        if objGrad - rho * errorGrad > 0
+            if errorGrad ~= 0  
+                rho = (objGrad + rhoHat) /errorGrad ;
+            else
+                % :( nothing to do.
+            end
+        end
+        
     end
     
     %% Merit function definition
