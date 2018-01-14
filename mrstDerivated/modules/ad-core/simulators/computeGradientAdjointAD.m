76c867fbe05efb9eb2ecc744716241176fda102b
Andres Codas <andrescodas@itk.ntnu.no>
2018-01-14 11:37:21 -0200

    bugfix: Only the first step is considered in gradient computations

--- mrstDerivated/modules/ad-core/simulators/computeGradientAdjointAD.m
+++ mrstDerivated/modules/ad-core/simulators/computeGradientAdjointAD.m
@@ -118,7 +118,7 @@ Sensitivity with respect to initial conditions
     for k = 1:nc
         ck = schedule.step.control == k;
         for j = 1:ncv
-            tmp = gradstep{ck, j};
+            tmp = vertcat(gradstep{ck, j});
             gradients{j, k} = full(sum(cat(3,tmp{:}), 3));
         end
     end
